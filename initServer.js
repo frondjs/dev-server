18e9c7fa4225ea7ae896437e21ecfe31cc971e4d
Murat Gözel <murat@gozel.com.tr>
2021-02-08 14:20:46 +0300

    Type error fix.

--- initServer.js
+++ initServer.js
@@ -10,7 +10,7 @@ const Automerge = require('automerge')
 module.exports = function createServer({port, watch, silent, publicpath, ctx}) {
   // get update count from automerge
   const dbname = ctx.project.name + '-db.json'
-  const dbpath = path.join('~/.frondjs' dbname)
+  const dbpath = path.join('~/.frondjs', dbname)
   const db = fs.existsSync(dbpath) ? fs.readFileSync(dbpath, 'utf8') : null
   let doc = db
     ? Automerge.load(db)
